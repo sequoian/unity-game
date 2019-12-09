fcf0c9ab36d50cf3d4b913cc147b09ef2455a099
Zach <zach303@msn.com>
2019-12-08 21:40:28 -0800

    initial commit

---
+++ Assets/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
+		Vector2 movement = new Vector2(horizontal, vertical);
+		movement = movement.normalized * moveSpeed;
+
+		rb.AddForce(movement);
+	}
+}
